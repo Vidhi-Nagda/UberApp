f61b884e9c83ba5df538c58c5952714330eb3c8a
Vidhi Nagda <nagda.v@northeastern.edu>
2024-10-12 14:04:10 -0500

    Initial project files

---
+++ src/main/java/com/vidhi/project/uber/uberapp/UberRideApplication.java
@@ -0,0 +1,13 @@
+package com.vidhi.project.uber.uberapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberRideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidhi/project/uber/uberapp/UberRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidhi.project.uber.uberapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
